171637d4cc5f3493dfdbf437c8e4f2c79a1dcf06
jhonnatan suarez <jonathan.sm87@gmail.com>
2022-06-15 10:53:49 -0500

    primer commit

---
+++ src/users/auth.service.ts
@@ -0,0 +1,25 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Injectable()
+export class AuthService {
+    constructor(private userService: UsersService) {}
+
+    async signup(email: string, password: string) {
+        // See if email is in use
+        const users = await this.userService.find(email);
+        if (users.length) {
+            throw new BadRequestException('email in use'); 
+        }
+
+        // Hash the users password
+
+        // create a new user and save it
+
+        // Return the user
+    }
+
+    signin(){
+
+    }
+}
---
+++ src/users/users.controller.ts
@@ -0,0 +1,59 @@
+import { 
+    Body, 
+    Controller, 
+    Post, 
+    Get, 
+    Patch, 
+    Delete, 
+    Param, 
+    Query,
+    NotFoundException
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { UsersService } from './users.service';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
+
+
+@Controller('auth')
+@Serialize(UserDto)
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    this.userService.create(body.email, body.password);
+  }
+
+  // @UseInterceptors(new SerializeInterceptor(UserDto))
+ //  @Serialize(UserDto)
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+      console.log('handler is running');
+      const user = await this.userService.findOne(parseInt(id));
+      if (!user) {
+          throw new NotFoundException('user not found');
+      }
+
+      return user;
+  }
+
+
+  @Get()
+  findAllUsers(@Query('email') email:string) {
+      return this.userService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+      return this.userService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+      return this.userService.update(parseInt(id), body);
+  }
+
+
+}
